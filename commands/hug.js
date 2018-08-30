4a0efa63fc3da93bf9ca41373ac8fc067f99be43
crost?? <pancrafterszerocrafters@gmail.com>
2018-08-31 06:49:06 +1000

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -12,6 +12,7 @@ const randomhug = ['https://media1.tenor.com/images/49a21e182fcdfb3e96cc9d9421f8
 'https://pa1.narvii.com/5925/bfd5fdfa6132c17a1c768a88536afb0589f7aeb6_hq.gif',
 'https://media.giphy.com/media/wnsgren9NtITS/giphy.gif',
 'https://media.tenor.com/images/ac5a0c47918dece5e69c1cc9fbb416a9/tenor.gif',
+'https://i.gifer.com/CDtQ.gif',         
 ]
 
 module.exports.run = async (client, message, args, level) => {
