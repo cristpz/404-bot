583ae5d276efe8f58643ba90201947a58bbeafe0
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-01 10:47:11 +1000

    Update chuckle.js

--- commands/chuckle.js
+++ commands/chuckle.js
@@ -1,9 +1,6 @@
 const Discord = require('discord.js')
 
 module.exports.run = async(bot, message, args) => {
-let m421 = args.join(" ");
- if (!m421) return message.channel.send('Please define a name.')
- if (m421.length > 30) return message.channel.send(`I can't rate your waifu! It's over 30 characters!`)
  let result = Math.floor((Math.random() * 100) + 0);
 
    const happyrate = new Discord.RichEmbed()
