8195b7de2384fa0452402258a03fbc5579d2000f
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-02 04:21:32 +1000

    Update users.js

--- commands/users.js
+++ commands/users.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let embed = new Discord.RichEmbed()
   .setDescription(`Currently serving ${bot.users.size} users.`)
+  .setColor("#3a0be7");
   message.channel.send(embed)
     }
 
