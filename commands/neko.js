83124d17cc58e3cc8cff3f7a4a8734bbf775231c
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-02 02:53:06 +1000

    Update neko.js

--- commands/neko.js
+++ commands/neko.js
@@ -1,18 +1,20 @@
-const superagent = require("snekfetch");
-const Discord = require('discord.js')
+const Discord = require("discord.js");
+const superagent = require("superagent");
 
-module.exports.run = async (client, message, args, level) => {
-    if (!message.channel.nsfw) return message.channel.send(":warning: This channel is not marked as **NSFW**")
-    superagent.get('https://nekos.life/api/v2/img/neko')
-        .end((err, response) => {
-      const lewdembed = new Discord.RichEmbed()
-      .setDescription("Nya~!")
-      .setImage(response.body.url)
-      .setColor("#3a0be7")
-  message.channel.send(lewdembed);
-    })
+module.exports.run = async (bot, message, args) => {
+
+  let {body} = await superagent
+  .get(`https://nekos.life/api/neko`);
+
+  let catembed = new Discord.RichEmbed()
+  .setColor("#3a0be7")
+  .setTitle("Meow!")
+  .setImage(body.file);
+
+  message.channel.send(catembed);
+
 }
 
-    module.exports.help = {
-      name: "lewdneko"
+module.exports.help = {
+  name: "neko"
 }
