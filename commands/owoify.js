2dfa9b3c3aaa408f79c1ca2ab332cce1a77b6e22
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-02 09:23:14 +1000

    Update owoify.js

--- commands/owoify.js
+++ commands/owoify.js
@@ -2,11 +2,15 @@ const Discord = require("discord.js");
 const superagent = require("snekfetch");
 
 module.exports.run = async (bot, message, args) => {
-
-  let {body} = await superagent
-  .get(`https://nekos.life/api/v2/owoify`);
-  
-return message.channel.send(body.msg);
+  async run(message, [text]) {
+let owoified = text.replace(/r|l/g, "w").replace(/R|L/g, "W").replace(/owo/i, 'OwO').replace(/uwu/i, 'UwU');
+        let embed = new MessageEmbed()
+        .setColor('#3a0be7')
+        .addField('OwOified', owoified)
+        .addField('Original', text)
+        .setFooter(`Requested by: ${message.author.tag}`)
+        
+return message.send(embed);
 
 }
 
