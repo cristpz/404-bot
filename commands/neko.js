fd0a0939abded532f75fa10460582d586f1a8c63
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-02 03:01:01 +1000

    Update neko.js

--- commands/neko.js
+++ commands/neko.js
@@ -1,16 +1,24 @@
+const discord = require("discord.js");
 const superagent = require("snekfetch");
-const Discord = require('discord.js');
 
-module.exports.run = async (client, message, args, level) => {
-    superagent.get('https://nekos.life/api/neko')
-        .end((err, response) => {
-      const nekoembed = new Discord.RichEmbed()
-      .setDescription("Nya~!")
-      .setImage(response.body.url)
-      .setColor("#3a0be7")
-  message.channel.send(nekoembed);
-    })
-}
+
+module.exports.run = async (client, message, args) => {
+    if (args < 1) return message.reply(" :x: O.o you wanna hug yourself??");
+    let stats = await client.getStats();
+    stats.hug++;
+    client.saveStats(stats);
+    await client.snekfetch.get('https://nekos.life/api/hug')
+        .set('Key', 'dnZ4fFJbjtch56pNbfrZeSRfgWqdPDgf')
+        .then(r => message.channel.send(`${args} You got a hug from ${message.author.username} :heart:`,{
+            embed: {
+                color: client.getRandomColor(),
+                image: {
+                    url: r.body.url
+                }
+            }
+        }).catch(e => console.warn('wew tf happened here ' + e)));
+
+};
 
 module.exports.help = {
   name: "neko"
