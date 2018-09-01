ba96b9cbc53321de0c8ff8230e34bf2c272d6406
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-02 05:06:43 +1000

    Update fuck.js

--- commands/fuck.js
+++ commands/fuck.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const superagent = require("superagent");
 
 module.exports.run = async (bot, message, args) => {
-
+ if (!message.channel.nsfw) return message.channel.send(":warning: This channel is not marked as **NSFW**")
   let {body} = await superagent
   .get(`https://nekos.life/api/v2/img/anal`);
 
