29dec525f6c65c4cfcfb65c2fecf9f36c2236668
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-02 09:25:20 +1000

    Update owoify.js

--- commands/owoify.js
+++ commands/owoify.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 const superagent = require("snekfetch");
 
-module.exports.run = async (bot, message, args) => {
-  async run(message, [text]) {
+module.exports.run = async (bot, message, [text], args) => {
+  
 let owoified = text.replace(/r|l/g, "w").replace(/R|L/g, "W").replace(/owo/i, 'OwO').replace(/uwu/i, 'UwU');
         let embed = new MessageEmbed()
         .setColor('#3a0be7')
