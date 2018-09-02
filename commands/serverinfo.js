38c25dc3e41853a042e318ca181210c9864440b0
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-02 11:26:54 +1000

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -4,14 +4,14 @@ module.exports.run = async (bot, message, args) => {
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
     .setDescription("Server Information")
-    .setColor("#15f153")
+    .setColor("#3a0be7")
     .setThumbnail(sicon)
     .addField("Server Name", message.guild.name)
     .addField("Created On", message.guild.createdAt)
     .addField("You Joined", message.member.joinedAt)
     .addField("Total Members", message.guild.memberCount)
     .addField("Server Owner", message.guild.owner)
-    .addField("Server Region", message.guild.region)
+    .addField("Server Region", message.guild.region);
 
     message.channel.send(serverembed);
 }
