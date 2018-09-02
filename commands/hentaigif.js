482dcae79120f56a1efc0d52d84f377b350f53d9
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-02 21:49:16 +1000

    Create hentaigif.js

---
+++ commands/hentaigif.js
@@ -0,0 +1,17 @@
+const superagent = require("snekfetch");
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args, level) => {
+    if (!message.channel.nsfw) return message.channel.send(":warning: This channel is not marked as **NSFW**")
+    superagent.get('https://nekos.life/api/v2/img/Random_hentai_gif')
+        .end((err, response) => {
+      const hentaiembed = new Discord.RichEmbed()
+      .setImage(response.body.url)
+      .setColor("#3a0be7")
+  message.channel.send(hentaiembed);
+    })
+}
+
+    module.exports.help = {
+      name: "randomhentaigif"
+}
