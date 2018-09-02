fda51a01c904ed7e74fc39abf0d0e3a4250ae26a
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-02 23:21:34 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,8 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 let purple = botconfig.purple;
 
+var Jimp = require('jimp');
+
 fs.readdir("./commands/", (err, files) => {
 
   if(err) console.log(err);
@@ -1193,6 +1195,58 @@ Height: 1,285 mm (50.6 in)
 Curb weight: 1,274 kg (2,809 lb)
 
 Image Link: https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/HondaS2000-004.jpg/1280px-HondaS2000-004.jpg`);	 
+}
+	 if (cmd === `${prefix}sad`) {
+
+    if (!args[0]) {
+      return;
+    }
+    message.channel.startTyping()
+    var url = message.author.avatarURL;
+
+    Jimp.read(url).then(function (image) {
+
+      image.resize(1024, 1024, Jimp.RESIZE_BEZIER);
+
+
+      Jimp.loadFont(doRandomSize()).then(function (font) { // load font from .fnt file
+        // print a message on an image
+        //image.print(font, 2, 2, args.join(" "), Jimp.ALIGN_FONT_CENTER); // print a message on an image with text wrapped at width
+
+
+        image.greyscale()
+
+
+        /**image.greyscale()**/
+        image.print(font, 20, 960, args.join(" "), Jimp.ALIGN_FONT_CENTER).getBuffer(Jimp.MIME_JPEG, onBuffer)
+
+
+
+
+        let outputfile = "./output/" + Math.random().toString(36).substr(2, 5) + "sad." + image.getExtension(); // create a random name for the output file
+        image.write(outputfile, function () {
+          // upload file
+          message.channel.sendFile(outputfile).then(function () {
+            // delete file
+            fs.unlink(outputfile);
+            console.log("SUCCESS: " + message.author.username);
+            message.channel.stopTyping()
+          });
+        });
+
+      });
+
+
+
+
+    }).catch(function (err) {
+      console.error(err);
+    })
+
+    function onBuffer(err, buffer) {
+      if (err) throw err;
+      console.log(buffer);
+    }
 }
 });
 
