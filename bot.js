0920a8da81ee8e3d609b873e66c00ef928bac7d5
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-02 11:16:49 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,29 +84,6 @@ bot.on("message", async message => {
     reportschannel.send(reportEmbed);
 
     return;
-  }
-
-if(cmd === `${prefix}serverinfo`){
- let guild = msg.guild;
-    let name = guild.name;
-    let createdAt = moment(guild.createdAt).format('MMMM Do YYYY, h:mm:ss a');
-    let channels = guild.channels.size;
-    let owner = guild.owner.user.tag;
-    let memberCount = guild.memberCount;
-    let large = guild.large;
-    let iconUrl = guild.iconURL;
-    let region = guild.region;
-    let afk = msg.guild.channels.get(guild.afkChannelID) === undefined ? 'None' : msg.guild.channels.get(guild.afkChannelID).name;
-
-    const embed = new Discord.RichEmbed()
-        .setTitle('Guild Information')
-        .addField('Channels', `**Channel Count:** ${channels}\n**AFK Channel:** ${afk}`, true)
-        .addField('Members', `**Member Count:** ${memberCount}\n**Owner:** ${owner}\n**Owner ID:** ${guild.owner.id}`, true)
-        .addField('More', `**Created at:** ${createdAt}\n**Large Guild?:** ${large ? 'Yes' : 'No'}\n**Region:** ${region}`)
-        .setThumbnail(iconUrl)
-        .setTimestamp();
-
-   return message.channel.send(embed);
 }
 
 if(cmd === `${prefix}info`){
