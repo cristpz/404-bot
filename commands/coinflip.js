bcc146115c606446672a13b299f8e5c37d64b46c
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-03 05:48:54 +1000

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -7,8 +7,18 @@ module.exports.run = async (bot, message, args) => {
 
   let result = Math.floor(Math.random() * 2); //Randomely selects Heads or Tails
 
-  if (result == 0) return message.channel.send("TAILS");
-  else if (result == 1) return message.channel.send("HEADS");
+  if (result == 0)
+    let tailsembed = new Discord.RichEmbed()
+.setAuthor(message.author.tag)
+.setColor("3a0be7")
+.addField("Answer", "Tails");
+    return message.channel.send(tailsembed);
+  else if (result == 1) 
+    let headsembed = new Discord.RichEmbed()
+.setAuthor(message.author.tag)
+.setColor("3a0be7")
+.addField("Answer", "Heads");
+    return message.channel.send(headsembed);
   else {
     return message.channel.send(
       "Error Occurred, Flip again.\n\n **If this problem keeps arising, make sure you've joined my support server to report any issues with the bot**"
