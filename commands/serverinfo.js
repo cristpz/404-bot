4cd87ce79e7291c93cb96b478705f2bf23b78407
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-02 11:38:12 +1000

    Create serverinfo.js

---
+++ commands/serverinfo.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("Server Information")
+    .setColor("#3a0be7")
+    .setThumbnail(sicon)
+    .addField("Server Name", message.guild.name)
+    .addField("Created On", message.guild.createdAt)
+    .addField("You Joined", message.member.joinedAt)
+    .addField("Total Members", message.guild.memberCount)
+    .addField("Server Owner", message.guild.owner)
+    .addField("Server Region", message.guild.region);
+
+    message.channel.send(serverembed);
+}
+
+module.exports.help = {
+  name: "serverinfo"
+}
