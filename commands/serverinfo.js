2814e82e975078ea70f411a54ddccbedc5d88348
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-02 11:25:08 +1000

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,32 +1,21 @@
-const Discord = require('discord.js');
-const moment = require('moment');
+const Discord = require("discord.js");
 
-module.exports.run = async (client, msg, args) => {
-    if (!msg.guild.available) {
-        try{await msg.channel.send('Discord API Error');}catch(e) {logger.error(e)}
-        return;
-    }
-    let guild = msg.guild;
-    let name = guild.name;
-    let createdAt = moment(guild.createdAt).format('MMMM Do YYYY, h:mm:ss a');
-    let channels = guild.channels.size;
-    let owner = guild.owner.user.tag;
-    let memberCount = guild.memberCount;
-    let large = guild.large;
-    let iconUrl = guild.iconURL;
-    let region = guild.region;
-    let afk = msg.guild.channels.get(guild.afkChannelID) === undefined ? 'None' : msg.guild.channels.get(guild.afkChannelID).name;
+module.exports.run = async (bot, message, args) => {
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("Server Information")
+    .setColor("#15f153")
+    .setThumbnail(sicon)
+    .addField("Server Name", message.guild.name)
+    .addField("Created On", message.guild.createdAt)
+    .addField("You Joined", message.member.joinedAt)
+    .addField("Total Members", message.guild.memberCount)
+    .addField("Server Owner", message.guild.owner)
+    .addField("Server Region", message.guild.region)
 
-    const embed = new Discord.RichEmbed()
-        .setTitle('Guild Information')
-        .addField('Channels', `**Channel Count:** ${channels}\n**AFK Channel:** ${afk}`, true)
-        .addField('Members', `**Member Count:** ${memberCount}\n**Owner:** ${owner}\n**Owner ID:** ${guild.owner.id}`, true)
-        .addField('More', `**Created at:** ${createdAt}\n**Large Guild?:** ${large ? 'Yes' : 'No'}\n**Region:** ${region}`)
-        .setThumbnail(iconUrl)
-
-    try{await msg.channel.send({embed});}catch(e){logger.error(e)}
-};
+    message.channel.send(serverembed);
+}
 
 module.exports.help = {
- name: serverinfo
+  name:"serverinfo"
 }
