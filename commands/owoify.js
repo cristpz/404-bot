2b559546b2bcef47744d857a6edd051c0841f5bd
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-02 10:27:17 +1000

    Create owoify.js

---
+++ commands/owoify.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = require('nekos.life');
+const neko = new client();
+
+module.exports.run = async (bot, message, args) => {
+async function work() {
+  let owo = await neko.getSFWOwOify({text: 'Hello!'});
+  console.log(owo);
+}
+
+work();
+
+module.exports.help = {
+  name: "hi"
+}
