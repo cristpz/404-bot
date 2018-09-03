3627bea9fdd4ed5c89b9c34105126a8efc8cd761
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-04 00:45:00 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("${bot.users.size} users | 404info", {type: "WATCHING"});
+  bot.user.setActivity(`${bot.users.size} users | 404info`, {type: "WATCHING"});
 
 });
 		
