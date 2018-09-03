813e8c306a2dd68e4b02a8ca06fba7e815ee7986
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-04 00:48:13 +1000

    Create coins.js

---
+++ commands/coins.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+let coins = require("../coins.json");
+
+module.exports.run = async (bot, message, args) => {
+  //404coins
+  if(!coins[message.author.id]){
+    coins[message.author.id] = {
+      coins: 0
+    };
+  }
+
+  let uCoins = coins[message.author.id].coins;
+
+
+  let coinEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#3a0be7")
+  .addField("💸", uCoins);
+
+  message.channel.send(coinEmbed);
+}
+
+module.exports.help = {
+  name: "coins"
+}
