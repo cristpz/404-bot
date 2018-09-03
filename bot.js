d44f1fbcd6afc40ed5b78aaa6e1c7dac3c0d13d8
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-04 00:23:03 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ let statuses = ['${bot.users.size} users', '404info', 'Happy Birthday Mizu!'];
 bot.on('ready', () => {
 setInterval(function(){
 let status = statuses[Math.floor(Math.random()=statuses.length)];
-	bot.user.setPresence({ game: { name: status }, status: 'online' });
+	bot.user.setPresence({ activity: { name: status }, status: 'online' });
   }, 3000);
 });
 		
