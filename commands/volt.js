e78a8f59ecd386bcab598658f593e7449e1a8cab
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-04 13:09:41 +1000

    Update volt.js

--- commands/volt.js
+++ commands/volt.js
@@ -53,7 +53,7 @@ module.exports.run = async (bot, message, args) => {
 
     async function getSubReddit() {
       let memeSubs = [
-        "",
+        "volt",
       ]; //List of meme subreddits
 
       let randomSub = randomNumber(memeSubs.length); //Randomely selects a subreddit
