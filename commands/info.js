25c09174f429865683ac561046f880c2eabbcf6a
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-04 16:23:12 +1000

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,20 +2,17 @@ const Discord = require("discord.js");
 const package = require("../package.json");
 
 module.exports.run = async (bot, message, args) => {
-  let botinfo = new Discord.RichEmbed()
+  let botembed = new Discord.RichEmbed()
     .setTitle("Bot Information")
     .setColor("#3a0be7")
     .setThumbnail(bot.user.displayAvatarURL)
-    .addField(
-      "Name",
-      `[${bot.user.username}](https://discordbots.org/bot/468180436032421898)`
-    )
+    .addField("Name", `[${bot.user.username}](https://discordbots.org/bot/468180436032421898)`)
     .addField("Version", `${package.version}`)
     .addField("Running on", `${bot.guilds.size} servers`)
     .addField("Developed by", `<@306104099185623042>`)
     .addField("Created on", bot.user.createdAt);
-  message.channel.send(botinfo); 
+  message.channel.send(botembed); 
 
 module.exports.help = {
   name: "info"
-};
+}
