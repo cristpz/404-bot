ca755f68610aba100243fb1e168b23c456755d43
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-04 16:18:29 +1000

    Create info.js

---
+++ commands/info.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const package = require("../package.json");
+
+module.exports.run = async (bot, message, args) => {
+  let botinfo = new Discord.RichEmbed()
+    .setTitle("Bot Information")
+    .setColor("#3a0be7")
+    .setThumbnail(bot.user.displayAvatarURL)
+    .addField(
+      "Name",
+      `[${bot.user.username}](https://discordbots.org/bot/468180436032421898)`
+    )
+    .addField("Version", `${package.version}`)
+    .addField("Running on", `${bot.guilds.size} servers`)
+    .addField("Developed by", `<@306104099185623042>`)
+    .addField("Created on", bot.user.createdAt);
+  message.channel.send(botinfo); 
+
+module.exports.help = {
+  name: "info"
+};
