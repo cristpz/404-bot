1f70b709b23ef5cd4b0b017484f99a2d7e056fff
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-04 12:07:47 +1000

    Update suicide.js

--- commands/suicide.js
+++ commands/suicide.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   const suicideEmbed = new Discord.RichEmbed()
    .setColor("#3a0be7")
-   .setTitle(`${message.author.username}, you should definately try one of those before dying~!`)
+   .setTitle(`${message.author.username}, you should definately try one of those, you might change your mind~!`)
    .setImage('https://i.kym-cdn.com/photos/images/original/001/260/891/d86.gif')
    message.channel.send(suicideEmbed);
 
