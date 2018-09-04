bbdbb298559d02f46834baa504536b9f0095d9ad
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-04 11:40:08 +1000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+let prefix = '404';
 
 module.exports.run = async (bot, message, args) => {
             message.channel.send(new Date().getTime() - message.createdTimestamp + " ms currently.");
