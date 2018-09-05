73c25856939ff41c2a01175b71ef4bb66a8b0b08
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-06 04:04:07 +1000

    Update emoji.js

--- commands/emoji.js
+++ commands/emoji.js
@@ -2,7 +2,7 @@ exports.run = async (client, msg, [Name, ID]) => {
     const prefix = msg.guild.settings.prefix || client.config.prefix;
 
     msg.delete();
-    if (!Name) { return msg.channel.send("You need a name of an emote to search with, baka!"); }
+    if (!Name) { return msg.channel.send("You need a name of an emote to search with!"); }
     if (msg.content.slice(prefix.length).startsWith("react") && (!ID)) {
         return msg.channel.send("You need to specify a message's ID so that I can find it!").then(msg => { setTimeout(() => { msg.delete(); }, 4000); });
     }
