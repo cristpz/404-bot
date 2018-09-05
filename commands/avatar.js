07ea637fd11ff00809e484c5f86d961e94fe2cdb
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-06 05:23:23 +1000

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,4 +1,6 @@
-exports.run = async (client, msg, [user]) => {
+const discord = require("discord.js");
+
+module.exports.run = async (client, msg, [user]) => {
     var data = await client.funcs.userSearch(msg, {user: [user], name: this.help.name});
     
     if (data.valid !== false) { 
@@ -6,17 +8,6 @@ exports.run = async (client, msg, [user]) => {
     }
 };
   
-exports.conf = {
-    enabled: true,
-    runIn: ["text"],
-    aliases: [],
-    permLevel: 0,
-    botPerms: ["ATTACH_FILES"],
-    requiredFuncs: ["userSearch"]
-};
-  
-exports.help = {
+module.exports.help = {
     name: "avatar",
-    description: "Fetch a user's avatar!",
-    usage: "[user:str]"
 };
