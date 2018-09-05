47996ce7ef27a0b37c9f05af650a91cfe5699631
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-06 04:55:33 +1000

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,3 +1,5 @@
+const discord = require("discord.js");
+
 exports.run = async (client, message, [songID]) => {
     const voiceChannel = message.member.voiceChannel;
     if (!message.member.voiceChannel) { return message.channel.send("You are not in the voice channel."); }
