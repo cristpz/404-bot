e11f4b78364d1f7eafbc76988d9818217a74991b
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-06 05:29:27 +1000

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -1,24 +1,13 @@
-exports.run = async (client, msg) => {
+const discord = require("discord.js");
+
+module.exports.run = async (client, msg) => {
   if (!msg.guild.voiceConnection) { throw `I am not connected in a voice channel, please add some songs to the queue first with ${msg.guild.settings.prefix}queueadd`; }
-  if (msg.guild.voiceConnection.dispatcher.paused) { return msg.send("The stream is already paused, baka!"); }
+  if (msg.guild.voiceConnection.dispatcher.paused) { return msg.send("The queue is already paused."); }
 
   msg.guild.voiceConnection.dispatcher.pause();
   return msg.send("⏸ The queue is now paused.");
 };
 
-exports.conf = {
-  enabled: true,
-  runIn: ["text"],
-  aliases: [],
-  permLevel: 0,
-  botPerms: [],
-  requiredFuncs: [],
-};
-
-exports.help = {
-  name: "pause",
-  description: "Pauses the playlist.",
-  usage: "",
-  usageDelim: "",
-  extendedHelp: "",
+module.exports.help = {
+    name: "pause",
 };
