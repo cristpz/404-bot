42839a084dd5a66c30b098f739c39834d9aa4f71
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-06 06:36:52 +1000

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,4 +1,4 @@
-const discord = require("discord.js");
+const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 let user = message.mentions.users.first() || message.author;
