34dd2decf8af6910522cba2a029934f236380dfb
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-06 05:31:43 +1000

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,7 +1,9 @@
-exports.run = async (client, msg, [question, ...option]) => {
+const discord = require("discord.js");
+
+module.exports.run = async (client, msg, [question, ...option]) => {
     if (!question) { return msg.reply("You need to provide a question."); }
     else if (option.length < 2) { return msg.reply("You need to provide at least two options!"); }
-    else if (option.length > 25) { return msg.reply("Whoa! You have a giant list of options! Not even I can handle all of these!"); }
+    else if (option.length > 25) { return msg.reply("You have a giant list of options, I can't handle these!"); }
 
     var emote = ["✅", "❎", "☑", "✔", "❌", "✖", "⭕", "🔘"];
 
@@ -9,7 +11,7 @@ exports.run = async (client, msg, [question, ...option]) => {
     const embed = new client.methods.Embed()
         .setColor("#3a0be7")
         .setTimestamp()
-        .setDescription(`A poll has been started by ${msg.author.username}!`)
+        .setDescription(`A poll has been started by ${message.author}!`)
         .addField("Question: ", `${question}`);
 
     for (var x = 0; x < option.length; x++) { embed.addField(`Option ${x + 1} - ${emote[x]}:`, option[x]); }
@@ -18,18 +20,6 @@ exports.run = async (client, msg, [question, ...option]) => {
     for (var x = 0; x < option.length; x++) { message.react(emote[x]); }
 };
 
-exports.conf = {
-    enabled: true,
-    runIn: ["text"],
-    aliases: [],
-    permLevel: 0,
-    botPerms: [],
-};
-
-exports.help = {
+module.exports.help = {
     name: "poll",
-    description: "Poll users",
-    usage: "[question:str] [option:str][...]",
-    usageDelim: " | ",
-    humanUse: "(question)_(option1)_(option2)_(etc...->option5)"
 };
