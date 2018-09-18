4e5d23140441f91523168c65c351f00fd59b76a9
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-18 22:31:37 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1233,4 +1233,4 @@ Image Link: https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/HondaS2000
 };
 });
 
-bot.login(process.env.BOT_TOKEN);
+bot.login(process.env.NDY4MTgwNDM2MDMyNDIxODk4.DoJ95g.VEDrDBRGU_tRYFtX_ytgN0ngJx4);
