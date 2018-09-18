27bb264932fbde411170f40d52b34ef6c33046cb
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-19 01:42:30 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity(`${bot.users.size} users | 404help`, {type: "WATCHING"});
+  bot.user.setActivity(`gtasa tutorials | 404help`, {type: "WATCHING"});
 
 });
 		
