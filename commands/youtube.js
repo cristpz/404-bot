7f07684397eb4cc25ef7c83a2186b95407e44fc6
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-23 10:57:41 +1000

    Create youtube.js

---
+++ commands/youtube.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) => {
+let youtube = args.slice(1).join('+');
+    let link = `https://www.youtube.com/results?search_query=` + youtube;
+	message.channel.send(link);
+
+module.exports.help = {
+  name: "youtube"
+};
