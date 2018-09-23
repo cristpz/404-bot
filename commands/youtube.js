a45d2210b196b111a7894d5e50c17ae9fbe11c10
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-23 11:10:38 +1000

    Update youtube.js

--- commands/youtube.js
+++ commands/youtube.js
@@ -4,6 +4,7 @@ module.exports.run = async (bot, message, args) => {
 let youtube = args.slice(1).join('+');
     let link = `https://www.youtube.com/results?search_query=` + youtube;
 	message.channel.send(link);
+}
 
 module.exports.help = {
   name: "youtube"
