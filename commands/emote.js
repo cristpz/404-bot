ee13cb3439420ca62c04b140079e6a189c932213
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-23 12:25:47 +1000

    Update emote.js

--- commands/emote.js
+++ commands/emote.js
@@ -4,11 +4,11 @@ module.exports.run = (client, message, args, level) => {
   const emoji = client.emojis.get(args[0]);
  const emoteurl = emoji.url
   const emojiembed = new Discord.RichEmbed()
-  // .setAuthor("Emote Info"," https://discordemoji.com/assets/emoji/owo.png")
-  .addField("» Emote Name",emoji.name)
+  .setAuthor("Emote Info"," https://discordemoji.com/assets/emoji/owo.png")
+  .addField("Emote Name",emoji.name)
    .setThumbnail(emoteurl)
-    .addField("» Emote Id",emoji.id)
-    .addField("» Created At",emoji.createdAt)
+    .addField("Emote Id",emoji.id)
+    .addField("Created At",emoji.createdAt)
    
 if(isNaN(args[0])) return message.channel.send("Emote name must not be a number!")
   
