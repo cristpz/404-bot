ef6aa79398e7e4eccd901fed53eeb96f3b1c5a7c
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-23 10:54:43 +1000

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -3,22 +3,9 @@ const google = require("google");
 
 
 module.exports.run = async (client, message, args) => {
-
-    let google = args.slice(0).join('+');
-
-        let link = `https://www.google.com/search?q=` + google;
-        if(!link)return message.reply("Console error")
-        let embed = new Discord.RichEmbed()
-	
-    .setColor("#3a0be7")
-    .setTimestamp()
-	.addField("Searching for", `${args.slice(0).join(' ')}`)
-	.addField('Link', `${link}`)
-	.setFooter(`Requested By ${message.author}`, message.author.avatarURL);
-          
-	message.channel.send(embed);
-	message.author.send(`You have searched for ${link} in ${ message.guild.name}`);
-  
+ let google = args.slice(1).join('+');
+    let link = `https://www.google.com/search?q=` + google;
+	message.channel.send(link);
 }
 
 module.exports.help = {
