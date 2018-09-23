ac16ca452ac28dc7410f81ef0efdf1f4764f66cc
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-23 11:01:39 +1000

    Create weed.js

---
+++ commands/weed.js
@@ -0,0 +1,30 @@
+return message.channel.send("**Smoking!**").then(async msg => {
+                        setTimeout(() => {
+                            msg.edit('🚬');
+                        }, 500);
+                        setTimeout(() => {
+                            msg.edit('🚬 ☁ ');
+                        }, 700);
+                        setTimeout(() => {
+                            msg.edit('🚬 ☁☁ ');
+                        }, 900);
+                        setTimeout(() => {
+                            msg.edit('🚬 ☁☁☁ ');
+                        }, 1000);
+                        setTimeout(() => {
+                            msg.edit('🚬 ☁☁☁');
+                        }, 1100);
+                        setTimeout(() => {
+                            msg.edit('🚬 ☁☁');
+                        }, 1200);
+                        setTimeout(() => {
+                            msg.edit('🚬 ☁');
+                        }, 1300);
+                        setTimeout(() => {
+                            msg.edit(`**weeeeeeeeeeeeeeeeeeeeeeeeeeeeed**`);
+                        }, 1500);
+                        setTimeout(() => {
+                            msg.delete(`**yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet**`);
+                        }, 6000);
+                    });
+                };
