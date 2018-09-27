15fb03bbe28ad3961268433adf71a6ed9d4a4801
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-28 05:16:54 +1000

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -33,10 +33,14 @@ module.exports.run = async (client, message, args, ops) => {
 
   ops.active.set(message.guild.id, data);
 
-  async function play(client, ops, data) {
+
+  }
+
+
+  async function playStream(client, ops, data) {
     client.channels.get(data.queue[0].announceChannel).send(`Now Playing: ${data.queue[0].songTitle} | Requested By: ${data.queue[0].requester}`);
 
-    data.dispatcher = await data.connection.play(ytdl(data.queue[0].url, { filter: 'audioonly' }));
+    data.dispatcher = await data.connection.playStream(ytdl(data.queue[0].url, { filter: 'audioonly' }));
     data.dispatcher.guildID = data.guildID;
 
     data.dispatcher.once('finish', function() {
@@ -61,7 +65,6 @@ module.exports.run = async (client, message, args, ops) => {
       if (vc) vc.leave();
     }
   }
-}
 
 
 
