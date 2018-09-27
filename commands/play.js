0bfc49d08057b09457e862cd813cae15742fc5b5
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-28 05:05:19 +1000

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -5,23 +5,70 @@ module.exports.run = async (client, message, args, ops) => {
 
   if (!message.member.voiceChannel) return message.channel.send('Please connect to a voice channel first.');
 
-  if (message.guild.me.voiceChannel) return message.channel.send(`Sorry, the bot is already connected to a voice channel.`);
-
   if (!args[0]) return message.channel.send('Sorry, I cannot play music if there are no arguments mentioned in this command.');
 
   let validate = await ytdl.validateURL(args[0]);
 
-  if (!validate) return message.channel.send('Please input a valid url.');
-
   let info = await ytdl.getInfo(args[0]);
 
-  let connection = await message.member.voiceChannel.join();
+  if (!validate) return message.channel.send('Please input a valid url.');
 
-  let dispatcher = await connection.playStream(ytdl(args[0], { filter: 'audioonly'}));
+  let data = ops.active.get(message.guild.id) || {};
 
-  message.channel.send(`Now playing: ${info.title} | Requested by: ${message.author}`)
-}
+  if (!data.connection) data.connection = await message.member.voiceChannel.join();
+  if (!data.queue) data.queue = [];
+  data.guildID = message.guild.id;
+
+  data.queue.push{{
+    songTitle: info.title,
+    requester: message.author.tag,
+    url: args[0],
+    announceChannel: mesage.channel.id
+  }};
+
+  if (!data.dispatcher) play(client, ops, data);
+  else {
+    message.channel.send(`Added to Queue: ${info.title} | Requested By: ${message.author.id}`);
+  }
+
+  ops.active.set(message.guild.id, data);
+
+
+  }
+
+
+  async function play(client, ops, data) {
+    client.channels.get(data.queue[0].announceChannel).send(`Now Playing: ${data.queue[0].songTitle} | Requested By: ${data.queue[0].requester}`);
+
+    data.dispatcher = await data.connection.play(ytdl(data.queue[0].url, { filter: 'audioonly' }));
+    data.dispatcher.guildID = data.guildID;
+
+    data.dispatcher.once('finish', function() {
+      finish(client, ops, this);
+    });
+  }
+
+  function finish(client, ops, dispatcher) {
+
+    let fetchembed = ops.active.get(dispatcher.guildID);
+
+    fetched.queue.shift();
+
+    if (fetched.queue.length > 0) {
+      ops.active.set(dispatcher.guildID, fetched);
+
+      play(client, ops, fetched);
+    } else {
+      ops.active.delete(dispatcher.guildID);
+
+      let vc = cliend.guilds.get(dispatcher.guildID).me.voiceChannel;
+      if (vc) vc.leave();
+    }
+  }
+
+
 
 module.exports.help = {
   name: "play"
 }
+
