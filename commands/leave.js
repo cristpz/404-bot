d849c1028500804879cc5a8a25ff8007abbcbf9e
crost?? <pancrafterszerocrafters@gmail.com>
2018-09-28 01:04:49 +1000

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -4,9 +4,9 @@ const ytdl = require('ytdl-core');
 module.exports.run = async (bot, message, args) => {
   if (!message.member.voiceChannel) return message.channel.send('PLease connect to a voice channel first.');
 
-  if (!message.guild.me.voiceChannel) return message.channel.send('Sorry, the bot isn\'t connected to the guild.');
+  if (!message.guild.me.voiceChannel) return message.channel.send('Sorry, the bot is not connected to the guild.');
 
-  if (message.guild.me.voiceChannelID) return message.channel.send('Sorry, you aren\'t connected to the same voice channel.');
+  if (message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send('Sorry, you are not connected to the same voice channel.');
 
   message.guild.me.voiceChannel.leave();
 
