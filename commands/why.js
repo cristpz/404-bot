777de291c0b3129a9c483c24223bc9df1135297a
crost?? <pancrafterszerocrafters@gmail.com>
2018-10-03 19:03:40 +1000

    Update why.js

--- commands/why.js
+++ commands/why.js
@@ -1,20 +1,18 @@
 const Discord = require("discord.js");
 const client = require('nekos.life');
-const neko = new client();
 const superagent = require("superagent");
 
 
 
 module.exports.run = async (client, message, args) => {
   let {body} = await superagent
-  .get(`https://nekos.life/api/v2/img/hug`);
+  .get(`https://nekos.life/api/v2/why`);
 
-  let hugembed = new Discord.RichEmbed()
+  let whyembed = new Discord.RichEmbed()
   .setColor("#3a0be7")
-  .setTitle(`${message.author} why though`)
-  .setImage(r.body.text);
+  .addField(`${message.author} why though`, r.body.text);
 
-  message.channel.send(hugembed);
+  message.channel.send(whyembed);
 
 }
 
