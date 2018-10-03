1806a35bc4bb36877cdff86060e19453f55e7181
crost?? <pancrafterszerocrafters@gmail.com>
2018-10-03 18:40:41 +1000

    Update owoify.js

--- commands/owoify.js
+++ commands/owoify.js
@@ -1,20 +1,27 @@
 const Discord = require("discord.js");
 const client = require('nekos.life');
 const neko = new client();
-const superagent = require("superagent");
+const superagent = require("snekfetch");
 
 
 
 module.exports.run = async (bot, message, args) => {
- 
- let {body} = await superagent
-  .get(`https://nekos.life/api/v2/owoify`);
+    let stats = await client.getStats();
 
-  let owoembed = new Discord.RichEmbed()
-  .setColor("#3a0be7")
-  .setTitle(body.text)
-
-  message.channel.send(owoembed);
+    await client.snekfetch.get('https://nekos.life/api/v2/owoify')
+        .then(r => message.channel.send({
+            embed: {
+                color: 0x3a0be7,
+                author: {
+                    name: "owo",
+                    icon_url: client.user.avatarURL
+                },fields: [
+                    {
+                        name: message.author.username + "owo ",
+                        value: r.body.owoify
+                    }]
+            }
+        }).catch(e => console.warn('damn that is fucked up ' + e)));
 
  
 }
