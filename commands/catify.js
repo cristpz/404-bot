9d908c6f28c09a378cf65d709b94fa814f9fd180
crost?? <pancrafterszerocrafters@gmail.com>
2018-10-03 17:43:32 +1000

    Update catify.js

--- commands/catify.js
+++ commands/catify.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 const client = require('nekos.life');
 const neko = new client();
 
-neko.getSFWCatText().then((catText) => console.log(catText));
+
 
 module.exports.run = async (bot, message, args) => {
 
- neko.getSFWCatText().then((catText) => message.channel.send(catText));
+ neko.getSFWCatText().then((catText) => console.log(catText));
 
 }
 
