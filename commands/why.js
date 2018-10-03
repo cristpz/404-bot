ddfd8e2039ea982c20143892b54c527fb5dfe34f
crost?? <pancrafterszerocrafters@gmail.com>
2018-10-03 18:52:28 +1000

    Update and rename owoify.js to why.js

--- commands/why.js
+++ commands/why.js
@@ -9,24 +9,23 @@ module.exports.run = async (client, message, args) => {
     let stats = await client.getStats();
     stats.why++;
     client.saveStats(stats);
-    await client.snekfetch.get('https://nekos.life/api/v2/owoify')
+    await client.snekfetch.get('https://nekos.life/api/why')
         .then(r => message.channel.send({
             embed: {
-                color: 0x3a0be7,
+                color: client.getRandomColor(),
                 author: {
-                    name: "owo",
+                    name: "Just why...",
                     icon_url: client.user.avatarURL
                 },fields: [
                     {
-                        name: message.author.username + "owo ",
-                        value: r.body.owoify
+                        name: message.author.username + "Asks ",
+                        value: r.body.why
                     }]
             }
-        }).catch(e => console.warn('damn that is fucked up ' + e)));
+        }).catch(e => console.warn('oh no ' + e)));
 
- 
 }
 
 module.exports.help = {
-  name: "owoify"
+  name: "why"
 }
