5de9b072261b745ad7edeb0ce6ba60ef39eac5b7
crost?? <pancrafterszerocrafters@gmail.com>
2018-10-04 22:57:40 +1000

    Update special.js

--- commands/special.js
+++ commands/special.js
@@ -1,14 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  
+message.channel.send(`
 
-let specialembed = new Discord.RichEmbed()
-.setAuthor(message.author.tag)
-.setColor("3a0be7")
-.setDescription("I am on twitter now!")
-.setURL("https://twitter.com/CristPZ/status/1047828470166953984");
+I am on twitter now! :wave:
 
-message.channel.send(specialembed);
+https://twitter.com/CristPZ/status/1047828470166953984`);
 
 
 
