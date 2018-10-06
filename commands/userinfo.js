9aebd3e911056a6e673e78bd928562bf241ca9a1
crost?? <pancrafterszerocrafters@gmail.com>
2018-10-06 11:30:32 +1000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,31 +1,36 @@
 const Discord = module.require('discord.js');
+const moment = require("moment");
+require("moment-duration-format");
+const status = {
+  online: "Online",
+  idle: "Idle",
+  dnd: "Do Not Disturb",
+  offline: "Offline/Invisible"
+};
 
 module.exports.run = async (bot, message, args) => {
-  let memberInfo = message.mentions.members.first();
-
-  if(!memberInfo){
-    var userinf = new Discord.RichEmbed()
-        .setThumbnail(message.author.avatarURL)
-        .setColor(0x3a0be7)
-        .addField("Full Username:", `${message.author.username}#${message.author.discriminator}`)
-        .addField("ID:", message.author.id)
-        .addField("Joined at:", message.member.joinedAt)
-        .addField("Created at:", message.author.createdAt);
-
-        message.channel.send(userinf);
-
-  }else{
-
-    var userinfo = new Discord.RichEmbed()
-        .setThumbnail(memberInfo.user.avatarURL)
-        .setColor(0x3a0be7)
-        .addField("Full Username:", `${memberInfo.user.username}#${memberInfo.user.discriminator}`)
-        .addField("ID:", memberInfo.id)
-        .addField("Joined at", message.isMemberMentioned.joinedAt)
-        .addField("Created at:", memberInfo.user.createdAt);
-
-        message.channel.send(userinfo);
+  const member = msg.mentions.members.first() || msg.guild.members.get(args[0]) || msg.member;
+  if (!member) return msg.reply("Please provide a valid username.");
+  let bot;
+  if (member.user.bot === true) {
+    bot = "Yes";
+  } else {
+    bot = "No";
   }
+  const userinfoembed = new Discord.MessageEmbed()
+    .setColor("#3a0be7")
+    .setThumbnail(`${member.user.displayAvatarURL()}`)
+    .setAuthor(`${member.user.tag} (${member.id})`, `${member.user.avatarURL()}`)
+    .addField("Nickname:", `${member.nickname !== null ? `Nickname: ${member.nickname}` : "No nickname"}`, true)
+    .addField("Bot?", `${bot}`, true)
+    .addField("Guild", `${bot}`, true)
+    .addField("Status", `${status[member.user.presence.status]}`, true)
+    .addField("Playing", `${member.user.presence.game ? `${member.user.presence.game.name}` : "nothing currently."}`, true)
+    .addField("Roles", `${member.roles.filter(r => r.id !== msg.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
+    .addField("Joined At", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
+    .addField("Created At", `${moment.utc(member.user.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true);
+
+  message.channel.send(userinfoembed);
 }
 
 module.exports.help = {
