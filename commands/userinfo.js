8f058976ab45eedf236e39126d9e8cdaac52b731
crost?? <pancrafterszerocrafters@gmail.com>
2018-10-06 12:09:18 +1000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -15,11 +15,11 @@ let user = message.mentions.users.first() || message.author;
   let userinfoembed = new Discord.RichEmbed()
     .setColor("#3a0be7")
     .setThumbnail(`${user.displayAvatarURL}`)
-    .setAuthor(`${user.tag} (${user.id})`, `${user.avatarURL}`)
+    .setAuthor(`${user.user.tag} (${user.id})`, `${user.avatarURL}`)
     .addField("Nickname:", `${user.nickname !== null ? `Nickname: ${user.nickname}` : "No nickname"}`, true)
     .addField("Status", `${status[user.presence.status]}`, true)
     .addField("Playing", `${user.presence.game ? `${user.presence.game.name}` : "nothing currently."}`, true)
-    .addField("Roles", `${user.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
+    .addField("Roles", `${user.user.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
     .addField("Joined At", `${moment.utc(user.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
     .addField("Created At", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true);
 
