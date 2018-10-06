bacb0c4474ac49c6e4ab71465457128f0c93b29e
crost?? <pancrafterszerocrafters@gmail.com>
2018-10-06 12:05:34 +1000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,5 +1,8 @@
 const Discord = module.require('discord.js');
-const moment = require("moment");
+
+
+module.exports.run = async (bot, message, args) => {
+  const moment = require("moment");
 require("moment-duration-format");
 const status = {
   online: "Online",
@@ -7,20 +10,18 @@ const status = {
   dnd: "Do Not Disturb",
   offline: "Offline/Invisible"
 };
-
-module.exports.run = async (bot, message, args) => {
 let user = message.mentions.users.first() || message.author;
   
   let userinfoembed = new Discord.RichEmbed()
     .setColor("#3a0be7")
     .setThumbnail(`${user.displayAvatarURL}`)
-    .setAuthor(`${user.user.tag} (${user.id})`, `${user.avatarURL}`)
+    .setAuthor(`${user.tag} (${user.id})`, `${user.avatarURL}`)
     .addField("Nickname:", `${user.nickname !== null ? `Nickname: ${user.nickname}` : "No nickname"}`, true)
-    .addField("Status", `${status[user.user.presence.status]}`, true)
-    .addField("Playing", `${user.user.presence.game ? `${user.user.presence.game.name}` : "nothing currently."}`, true)
-    .addField("Roles", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
+    .addField("Status", `${status[user.presence.status]}`, true)
+    .addField("Playing", `${user.presence.game ? `${user.presence.game.name}` : "nothing currently."}`, true)
+    .addField("Roles", `${user.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
     .addField("Joined At", `${moment.utc(user.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
-    .addField("Created At", `${moment.utc(user.user.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true);
+    .addField("Created At", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true);
 
   message.channel.send(userinfoembed);
 }
