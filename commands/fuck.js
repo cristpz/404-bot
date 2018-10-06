3bc34c55fa96d59898bf4d704c7ff2ddcdcbf136
crost?? <pancrafterszerocrafters@gmail.com>
2018-10-06 23:47:21 +1000

    Create fuck.js

---
+++ commands/fuck.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) => {
+ if (!message.channel.nsfw) return message.channel.send(":warning: This channel is not marked as **NSFW**")
+  let {body} = await superagent
+  .get(`https://nekos.life/api/v2/img/classic`);
+
+  let fuckembed = new Discord.RichEmbed()
+  .setColor("#3a0be7")
+  .setTitle(`${message.author.username} fucked ${message.mentions.members.first().user.username}`)
+  .setImage(body.url);
+
+  message.channel.send(fuckembed);
+
+}
+
+module.exports.help = {
+  name: "fuck"
+}
