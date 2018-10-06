09ee7a4198c5b7f22be6cd32a1ffb0a04b0d63bd
crost?? <pancrafterszerocrafters@gmail.com>
2018-10-06 12:01:05 +1000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -9,19 +9,18 @@ const status = {
 };
 
 module.exports.run = async (bot, message, args) => {
-  let member = message.mentions.users.first() || message.author;
-  if (!member) return message.reply("Please provide a valid username.");
+let user = message.mentions.users.first() || message.author;
   
   let userinfoembed = new Discord.RichEmbed()
     .setColor("#3a0be7")
-    .setThumbnail(`${member.user.displayAvatarURL}`)
-    .setAuthor(`${member.user.tag} (${member.id})`, `${member.user.avatarURL}`)
-    .addField("Nickname:", `${member.nickname !== null ? `Nickname: ${member.nickname}` : "No nickname"}`, true)
-    .addField("Status", `${status[member.user.presence.status]}`, true)
-    .addField("Playing", `${member.user.presence.game ? `${member.user.presence.game.name}` : "nothing currently."}`, true)
+    .setThumbnail(`${user.displayAvatarURL}`)
+    .setAuthor(`${user.user.tag} (${user.id})`, `${user.user.avatarURL}`)
+    .addField("Nickname:", `${user.nickname !== null ? `Nickname: ${user.nickname}` : "No nickname"}`, true)
+    .addField("Status", `${status[user.user.presence.status]}`, true)
+    .addField("Playing", `${user.user.presence.game ? `${user.user.presence.game.name}` : "nothing currently."}`, true)
     .addField("Roles", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
-    .addField("Joined At", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
-    .addField("Created At", `${moment.utc(member.user.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true);
+    .addField("Joined At", `${moment.utc(user.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
+    .addField("Created At", `${moment.utc(user.user.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true);
 
   message.channel.send(userinfoembed);
 }
