d38abd9422483fa43d277ee92534fe44652aad68
crost?? <pancrafterszerocrafters@gmail.com>
2018-10-06 11:37:37 +1000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -17,8 +17,6 @@ module.exports.run = async (bot, message, args) => {
     .setThumbnail(`${member.user.displayAvatarURL()}`)
     .setAuthor(`${member.user.tag} (${member.id})`, `${member.user.avatarURL()}`)
     .addField("Nickname:", `${member.nickname !== null ? `Nickname: ${member.nickname}` : "No nickname"}`, true)
-    .addField("Bot?", `${bot}`, true)
-    .addField("Guild", `${bot}`, true)
     .addField("Status", `${status[member.user.presence.status]}`, true)
     .addField("Playing", `${member.user.presence.game ? `${member.user.presence.game.name}` : "nothing currently."}`, true)
     .addField("Roles", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
