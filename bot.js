f825cfa1bea70f3200acb015257f3392488e38bd
Crist <pancrafterszerocrafters@gmail.com>
2018-10-08 07:22:29 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,20 @@ bot.on("message", async message => {
   }
 	 if(cmd === `${prefix}specialcuddle`){
   if(message.author.id !== botconfig.ownerID) return;
+  let specialcuddleembed = new Discord.RichEmbed()
+     .setTitle(`${message.author}, here have a cuddle from me ^-^`)
+     .setImage("https://cdn.discordapp.com/attachments/463023443571507210/498584775855177729/BiodegradableFluffyBluefish-size_restricted-9.png");
+  message.channel.send(specialcuddleembed);
+  }
+		 if(cmd === `${prefix}specialcuddle`){
   if(message.author.id !== botconfig.glidelasID) return;
+  let specialcuddleembed = new Discord.RichEmbed()
+     .setTitle(`${message.author}, here have a cuddle from me ^-^`)
+     .setImage("https://cdn.discordapp.com/attachments/463023443571507210/498584775855177729/BiodegradableFluffyBluefish-size_restricted-9.png");
+  message.channel.send(specialcuddleembed);
+  }
+		 if(cmd === `${prefix}specialcuddle`){
+  if(message.author.id !== botconfig.chuckleID) return;
   let specialcuddleembed = new Discord.RichEmbed()
      .setTitle(`${message.author}, here have a cuddle from me ^-^`)
      .setImage("https://cdn.discordapp.com/attachments/463023443571507210/498584775855177729/BiodegradableFluffyBluefish-size_restricted-9.png");
