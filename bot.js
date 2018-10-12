f56cfb69527ca6cd9daff8217c78c66a284c1537
Crist <pancrafterszerocrafters@gmail.com>
2018-10-13 00:26:12 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,7 +63,7 @@ bot.on("message", async message => {
   message.channel.send(specialcuddleembed);
   }
 if (message.isMentioned(bot.user)) {
-    message.reply(`my prefix is **404**, use **404help** for more commands!`);
+    message.reply('my prefix is `404`, use `404help` for more commands!');
 }
   if(cmd === `${prefix}report`){
 
