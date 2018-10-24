1f347de9b1d9796d945a7649dacfc699b52b64b1
Crist <pancrafterszerocrafters@gmail.com>
2018-10-25 06:34:27 +1100

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -2,11 +2,7 @@ const Discord = require("discord.js");
 const Report = require("../models/suggest.js")
 const mongoose = require("mongoose");
 mongoose.connect('mongodb+srv://404:404-2409@404bot-3efij.mongodb.net/test?retryWrites=true/suggestions');
-mongoose.connection.once('open', function(){
-  console.log('Connection successful!');
-}).on('error', function(error){
-  console.log('Ah heck! Error Code:', error);
-});
+
 module.exports.run = async (bot, message, args) => {
   await message.delete();
   let suggestion = args.slice(0).join(" ");
