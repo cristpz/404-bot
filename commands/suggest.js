88a3f188b90e22ecb4787aee25171ab815275e0a
Crist <pancrafterszerocrafters@gmail.com>
2018-10-25 06:54:07 +1100

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,26 +1,13 @@
 const Discord = require("discord.js");
-const Report = require("../models/suggest.js")
-const mongoose = require("mongoose");
-mongoose.connect('mongodb+srv://404:404-2409@404bot-3efij.mongodb.net/test?retryWrites=true/suggestions');
 
 module.exports.run = async (bot, message, args) => {
   await message.delete();
   let suggestion = args.slice(0).join(" ");
   if (!suggestion) return message.reply("Please enter a valid suggestion!");
-
-  const report = new Report({
-    _id: mongoose.Types.ObjectId(),
-    username: rUser.user.username,
-    userID: rUser.id,
-    suggestion: suggestion,
-    time: message.createdAt
-  });
-
-  report.save()
-  .then(result => console.log(result))
-  .catch(err => console.log(err));
-
-  message.reply("That suggestion has been saved to the database, thank you!")
+  let guildID = "500244270373011466";
+  let channelID = "504743305427550219"
+bot.guilds.get(guildID).channels.get(channelID).send(suggestion + `by ${message.author.username} with ID ${message.author.id} at ${message.createdAt}`)
+  message.reply("That suggestion has been saved, thank you!")
 
 }
 
