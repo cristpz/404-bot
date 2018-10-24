f5b0713a50ea49fbfa0acc5f384e71f46eb746da
Crist <pancrafterszerocrafters@gmail.com>
2018-10-25 07:23:44 +1100

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -4,16 +4,15 @@ module.exports.run = async (bot, message, args) => {
   await message.delete();
   let suggestion = args.slice(0).join(" ");
   if (!suggestion) return message.reply("Please enter a valid suggestion!");
-  let guildID = "500244270373011466";
   let channelID = "504743305427550219";
   let suggestembed = new Discord.RichEmbed
   .setAuthor(message.author.username)
   .addField("With ID", message.author.id)
   .addField("Time/Date", message.createdAt)
   .setColor("3a0be7");
-bot.guilds.get(guildID).channels.get(channelID).send(suggestembed);
+client.channels.get(channelID).send(suggestembed);
   
-  message.reply("That suggestion has been saved, thank you!");
+//  message.reply("That suggestion has been saved, thank you!");
 
 }
 
