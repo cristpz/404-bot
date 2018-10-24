8fe738e1c1eeb1d28ec9a9eba2d69f665b1f373c
Crist <pancrafterszerocrafters@gmail.com>
2018-10-25 07:12:12 +1100

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -11,7 +11,7 @@ module.exports.run = async (bot, message, args) => {
   .addField("With ID", message.author.id)
   .addField("Time/Date", message.createdAt)
   .setColor("3a0be7");
-client.guilds.get(guildID).channels.get(channelID).send(suggestembed);
+bot.guilds.get(guildID).channels.get(channelID).send(suggestembed);
   
   message.reply("That suggestion has been saved, thank you!");
 
