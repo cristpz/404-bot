8a664bb2d626ec790ff147abde09e93bf1fc7015
Crist <pancrafterszerocrafters@gmail.com>
2018-10-26 08:32:30 +1100

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   message.channel.send(`
-  `Server Invite Link:`
+  \`Server Invite Link:\`
   https://discord.gg/tnPXsXw
   
-  **First** `50` **members will get a** `special role` **after we hit** `100 members` **, cheers!**`);
+  The first **50** members will get a **special role** after we hit 100 members, cheers lads.`);
 
 }
 
