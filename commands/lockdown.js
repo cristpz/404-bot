433e404599d0c0022d8304752fdf8984583e6c1a
Crist <pancrafterszerocrafters@gmail.com>
2018-10-29 09:17:38 +1100

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -12,8 +12,8 @@ module.exports.run = (client, message, args) => {
             SEND_MESSAGES: null
         }).then(() => {
             message.channel.sendMessage('**Lockdown lifted.**');
-            clearTimeout(client.lockit[message.channel.id]);
-            delete client.lockit[message.channel.id];
+            clearTimeout(bot.lockit[message.channel.id]);
+            delete bot.lockit[message.channel.id];
         }).catch(error => {
             console.log(error);
         });
@@ -23,11 +23,11 @@ module.exports.run = (client, message, args) => {
         }).then(() => {
             message.channel.send(`**Channel locked** for ${ms(ms(time), { long:true })}.`).then(() => {
 
-                client.lockit[message.channel.id] = setTimeout(() => {
+                bot.lockit[message.channel.id] = setTimeout(() => {
                     message.channel.overwritePermissions(message.guild.id, {
                         SEND_MESSAGES: null
                     }).then(message.channel.send('**Lockdown lifted.**')).catch(console.error);
-                    delete client.lockit[message.channel.id];
+                    delete bot.lockit[message.channel.id];
                 }, ms(time));
 
             }).catch(error => {
